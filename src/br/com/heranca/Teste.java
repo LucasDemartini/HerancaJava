fb0cd622328a97f98f934deac211e5a9868ab527
LucasDemartini <Lucas.demartini@institutoivoti.com.br>
2017-10-03 20:46:46 -0300

    Criação dos testes

---
+++ src/br/com/heranca/Teste.java
@@ -0,0 +1,14 @@
+package br.com.heranca;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Teste {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
